05c4d4cfafccaaf67eaca5b1a02d70e2251cb8bd
yossi <yossitrx@gmail.com>
2016-05-04 20:33:17 +0300

    Update

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var Students = require("./students");
-//var eventConfig = require("./config");
 
 var app = express(); 
 
@@ -20,23 +19,34 @@ app.all('*',function (req,res,next){
 	req.next();
 });
 //show all students 
+//config the url students
 app.get('/students',function(req,res){
+	//console log successfull connection
 	console.log("connection request 'show all students' was successfull");
+	//activiting method to search by all students
 	res.send(StudentsController.showAllStudents());
 	res.end();
 });
 
 //show students by id 
+//config the url Id
 app.get('/showId/:id',function(req,res){
+	//getting from url the Id
 	var id = req.params.id;
+	//console log successfull connection
 	console.log("connection request 'show student by Id' was successfull");
+	//activiting method to search by Id
 	res.send(StudentsController.showStudentsById(id));
 	res.end();
 });
 //show student by year
+//config the url Year
 app.get('/showYear/:year',function(req,res){
+	//getting from url the year
 	var year = req.params.year;
+	//console log successfull connection
 	console.log("connection request 'show student by year' was successfull");
+	//activiting method to search by year
 	res.send(StudentsController.showStudentsByYear(year));
 	res.end();
 });
