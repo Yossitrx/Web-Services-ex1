04326d25d40b3fe19243b4d3a11338541c64c8a5
yossi <yossitrx@gmail.com>
2016-05-04 20:26:40 +0300

    Update

--- students/index.js
+++ students/index.js
@@ -1,14 +1,15 @@
 'use strict'
 
 var fileSystem = require("fs");
+//requireing json data
 var studentData = ("./students/studentsData.json");
 
-
+//ES6 student class
 module.exports = class student {
-
 	constructor(){
 		
 	}
+	//parsing all students from json and returning them plus console log them
 	showAllStudents(){
 		console.log("Show All students");
 		var data = fileSystem.readFileSync(studentData);
@@ -16,6 +17,8 @@ module.exports = class student {
 		var jsonData = JSON.parse(data);
 		return jsonData;
 	}
+	//parsing all students from json then searching for exact match with ID and returning the student
+	//plus console log ID
 	showStudentsById(id){
 		console.log("Show student by Id");
 		var data = fileSystem.readFileSync(studentData);
@@ -29,8 +32,11 @@ module.exports = class student {
 				return student;
 			}		
 		}
+		//If ID not not exist console will display + empty array in webPage
 		console.log("student: " + id + ", not exist");		
 	}
+    //parsing all students from json then searching for exact match with Year and returning and students that have this year 
+	//plus console log students from that year
 	showStudentsByYear(year){
 		console.log("Show student By year: ");
 		var data = fileSystem.readFileSync(studentData);
